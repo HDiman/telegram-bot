de779a67f74f8b2377cde569b5ae9040e5d51134
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-06-14 11:05:16 +0300

    Telegram Bot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from datetime import datetime
+import telebot
+from auth_data import token
+
 
 def get_data():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
@@ -9,7 +12,15 @@ def get_data():
     print(response)
     print(f"{today_day}\n Sell BTC price: {sell_price}\n")
 
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["Start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello friend! Write the 'price' to find out the sell price of BTC")
+
 
 if __name__ == "__main__":
-    get_data()
+    # get_data()
+    telegram_bot(token)
 
